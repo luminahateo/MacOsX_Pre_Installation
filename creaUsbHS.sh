a813308f18298b42f027394b33c377593d11339c
Hateo Lumina <29329710+luminahateo@users.noreply.github.com>
2021-06-26 12:29:02 +0200

    vers 1.1
    rajout crea Usb

---
+++ creaUsbHS.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#CREACTION D'UNE CLEF USB MAC OS X HIGH SIERRA
+####################################################################################################################################################################################################################################################################
+
+sudo /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume
